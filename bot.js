bf9f4d45ed1bcb1cc20487f65a286625cd46aa5c
Oscar Berry <oscar@madokami.com>
2016-08-03 12:19:56 +0100

    emo bot

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ const config = require('./config');
 const util = require('util');
 const Utils = require('./lib/Utils');
 const request = require('request');
-const knex = require('knex')(config.knexOptions);
 const fs = require('fs');
 const moment = require('moment');
 const numeral = require('numeral');
@@ -22,26 +21,12 @@ const CustomCommandHandler = require('./lib/CustomCommandHandler');
 const VoiceSession = require('./lib/VoiceSession');
 const querystring = require('querystring');
 
-var voiceSessions = new Map();
-
-var logger = new MessageLogger(knex);
-
 var bot = new Eris(config.token, config.erisOptions);
 
 bot.on('ready', () => {
     console.log('Username: ' + bot.user.username);
 
     console.log('Servers:');
-
-    bot.guilds.forEach((server) => {
-        console.log('\t' + server.name);
-
-        logger.batchInsertChannels(server.channels);
-    });
-
-    bot.users.forEach((user) => {
-        logger.insertUpdateUser(user);
-    });
 
     console.log();
 });
@@ -50,63 +35,10 @@ bot.on('error', (e) => {
     console.log(e);
 });
 
-bot.on('messageCreate', (message) => {
-    logger.handle(bot, message);
-});
-
-bot.on('messageUpdate', (message, oldMessage) => {
-    logger.handle(bot, message);
-});
-
 var commands = new CommandHandler();
 bot.on('messageCreate', (message) => {
     commands.handle(bot, message);
 });
-
-bot.on('userUpdate', (user) => {
-    logger.insertUpdateUser(user);
-});
-
-bot.on('guildMemberAdd', (user) => {
-    logger.insertUpdateUser(user);
-});
-
-/*
-commands.command('enable', (command) => {
-    command.description = 'Enable the bot on this channel.';
-
-    command.args((args) => {
-        args.channel().optional();
-    });
-
-    command.handler = (bot, message) => {
-        knex(constants.TABLE_CHANNEL_WHITELIST).insert({
-            channel_id: message.channel.id
-        }).then(() => {
-            bot.createMessage(message.channel.id,
-                util.format('Self-bot enabled on **%s**', Utils.formatChannelMention(message.channel.id)));
-        });
-    };
-});
-
-commands.command('disable', (command) => {
-    command.description = 'Disable the bot on this channel.';
-
-    command.args((args) => {
-        args.channel().optional();
-    });
-
-    command.handler = (bot, message) => {
-        knex(constants.TABLE_CHANNEL_WHITELIST)
-            .where('channel_id', message.channel.id)
-            .delete()
-            .then(() => {
-                bot.createMessage(message.channel.id,
-                    util.format('Self-bot disabled on **%s**', Utils.formatChannelMention(message.channel.id)));
-            });
-    };
-});
-*/
 
 commands.command('logs', (command) => {
 
@@ -117,6 +49,22 @@ commands.command('logs', (command) => {
             querystring.stringify({ channel: message.channel.id }));
 
         bot.createMessage(message.channel.id, url);
+    };
+
+});
+
+commands.command('hug', (command) => {
+
+    command.args((args) => {
+        args.user().optional();
+    });
+
+    command.handler = (bot, message, userId) => {
+        if(!userId) {
+            userId = message.author.id;
+        }
+
+        bot.createMessage(message.channel.id, util.format('*hugs %s*', Utils.formatUserMention(userId)));
     };
 
 });
