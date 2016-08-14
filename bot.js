6f5abb974a7185edd73289380de713c507a42d29
yuikitty <oscar@madokami.com>
2016-08-14 03:06:27 +0100

    Mute/unmute

--- bot.js
+++ bot.js
@@ -19,9 +19,10 @@ const numeral = require('numeral');
 const constants = require('./constants');
 const ChannelWhitelistGuard = require('./lib/Guards/ChannelWhitelistGuard');
 const UserWhitelistGuard = require('./lib/Guards/UserWhitelistGuard');
+const RoleWhitelistGuard = require('./lib/Guards/RoleWhitelistGuard');
 const Database = require('./lib/Database');
 const querystring = require('querystring');
-
+const CommandArgument = require('./lib/CommandArgument');
 
 var db = new Database(knex);
 var logger = new MessageLogger(db);
@@ -29,6 +30,12 @@ var logger = new MessageLogger(db);
 var userGuard = new UserWhitelistGuard([
     '175044949744680970',
     '163017409521909760'
+]);
+
+var roleGuard = new RoleWhitelistGuard([
+    '176844097292599297', // >>Admins
+    '198862394468990977', // Kneesocks
+    '210039704836374529' // test (SC)
 ]);
 
 var bot = new Eris(process.env.DISCORD_TOKEN || config.token, config.erisOptions);
@@ -40,6 +47,8 @@ bot.on('ready', () => {
 
     bot.guilds.forEach((server) => {
         console.log('\t' + server.name);
+
+        console.log(server.roles);
 
         db.batchInsertChannels(server.channels);
     });
@@ -92,7 +101,70 @@ bot.on('guildMemberAdd', (user) => {
     }
 });
 
+commands.command('game', (command) => {
+    command.description = 'Change the bots\'s currently playing game';
+
+    command.guards.add(roleGuard);
+
+    command.args((args) => {
+        args.argument('game').optional();
+    });
+
+    command.handler = (bot, message, game) => {
+        bot.editGame({ name: game });
+    };
+});
+
+commands.command('help', (command) => {
+    command.description = 'Show this help text';
+
+    command.handler = (bot, message) => {
+        var msg = [ ];
+
+        msg.push('Commands:');
+
+        commands.commands.forEach((command, name) => {
+            var args = [ ];
+
+            if(command.arguments) {
+                command.arguments.forEach(arg => {
+                    var str = null;
+
+                    if (arg.options.has(CommandArgument.OPTIONAL)) {
+                        str = '[' + arg.name + ']';
+                    }
+                    else {
+                        str = '<' + arg.name + '>';
+                    }
+
+                    args.push(str);
+                });
+            }
+
+            msg.push(util.format('.%s %s - %s', name, args.join(' '), command.description));
+        });
+
+        msg.push('');
+
+        db.getCustomCommandNames().then(rows => {
+            var names = [ ];
+
+            rows.forEach(row => {
+               names.push('.' + row.name);
+            });
+
+            if(names.length > 0) {
+                msg.push('Custom commands: ' + names.join(', '));
+            }
+
+            bot.createMessage(message.channel.id, msg.join('\n'));
+        });
+    };
+
+});
+
 commands.command('logs', (command) => {
+    command.description = 'Get the URL for the server message logs';
 
     command.handler = (bot, message) => {
         var url = util.format('%s/logs/%s?%s',
@@ -106,8 +178,9 @@ commands.command('logs', (command) => {
 });
 
 commands.command('commands.edit', (command) => {
+    command.description = 'Edit a custom command';
 
-    command.guards.add(userGuard);
+    command.guards.add(roleGuard);
 
     command.args((args) => {
         args.argument('name');
@@ -116,6 +189,79 @@ commands.command('commands.edit', (command) => {
 
     command.handler = (bot, message, name, content) => {
         db.insertUpdateCustomCommand(name, content);
+    };
+
+});
+
+commands.command('mute', (command) => {
+    command.description = 'Mute a user on the current channel';
+
+    command.guards.add(roleGuard);
+
+    command.args((args) => {
+        args.user();
+    });
+
+    command.handler = (bot, message, userId) => {
+        console.log(userId);
+
+        var allowMask = 0;
+        var denyMask = 0;
+
+        var channelId = message.channel.id;
+
+        var existingPermissions = message.channel.guild.channels.get(channelId).permissionOverwrites.find(function(perm) {
+            return perm.type === 'member' && perm.id === userId;
+        });
+
+        if(existingPermissions) {
+            allowMask = existingPermissions.allow;
+            denyMask = existingPermissions.deny;
+        }
+
+        denyMask |= Eris.Constants.Permissions.sendMessages;
+        allowMask &= ~Eris.Constants.Permissions.sendMessages;
+
+        bot.editChannelPermission(channelId, userId, allowMask, denyMask, 'member').then(function() {
+            bot.createMessage(message.channel.id, util.format('%s has been muted on %s',
+                Utils.formatUserMention(userId),
+                Utils.formatChannelMention(channelId)));
+        });
+    };
+
+});
+
+commands.command('unmute', (command) => {
+    command.description = 'Unmute a user on the current channel';
+
+    command.guards.add(roleGuard);
+
+    command.args((args) => {
+        args.user();
+    });
+
+    command.handler = (bot, message, userId) => {
+        var allowMask = 0;
+        var denyMask = 0;
+
+        var channelId = message.channel.id;
+
+        var existingPermissions = message.channel.guild.channels.get(channelId).permissionOverwrites.find(function(perm) {
+            return perm.type === 'member' && perm.id === userId;
+        });
+
+        if(existingPermissions) {
+            allowMask = existingPermissions.allow;
+            denyMask = existingPermissions.deny;
+        }
+
+        denyMask &= ~Eris.Constants.Permissions.sendMessages;
+
+        bot.editChannelPermission(channelId, userId, allowMask, denyMask, 'member').then(function() {
+            bot.createMessage(message.channel.id, util.format('%s has been unmuted on %s',
+                Utils.formatUserMention(userId),
+                Utils.formatChannelMention(channelId)));
+        }).catch(e => console.log(e));
     };
 
 });
